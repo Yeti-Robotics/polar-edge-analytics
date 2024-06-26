71444904a95a0bf8613d6e552f958fa72da2eef2
Drew Beamer <andrewmbeamer@gmail.com>
2024-06-25 20:53:38 -0400

    add svgs snowverlay

--- lib/components/decorative/snowverlay/index.stories.tsx
+++ lib/components/decorative/snowverlay/index.stories.tsx
@@ -7,10 +7,31 @@ const meta = {
 	title: "Components/Decorative/Snowverlay",
 	parameters: {
 		backgrounds: {
-			default: "yeti-blue",
-			values: [{ name: "yeti-blue", value: "#54B6E5" }],
+			default: "sunset",
+			values: [
+				{ name: "yeti-blue", value: "#54B6E5" },
+				{
+					name: "sunset",
+					value: "linear-gradient(to bottom right, #54B6E5 20%, rgba(217, 70, 239, 0.6), #fb923c 100%)",
+				},
+			],
 		},
 	},
+	argTypes: {
+		numSnowflakes: {
+			control: { type: "range", min: 0, max: 125, step: 5 },
+		},
+		blurFrequency: {
+			control: { type: "range", min: 0, max: 1, step: 0.1 },
+		},
+	},
+	decorators: [
+		(Story) => (
+			<div style={{ height: "100vh" }}>
+				<Story />
+			</div>
+		),
+	],
 } satisfies Meta<typeof Snowverlay>;
 
 export default meta;
