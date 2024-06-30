290770864f3ce878172b15fdf9541004d6548be0
Drew Beamer <andrewmbeamer@gmail.com>
2024-06-29 20:43:37 -0400

    stateless solution...all works with RSC

--- app/scout/page.tsx
+++ app/scout/page.tsx
@@ -1,3 +1,38 @@
+import { CounterInput } from "@/lib/components/forms/counter-input";
+import { Label } from "@/lib/components/ui/label";
+import { TabsContent, TabsList, TabsTrigger } from "@/lib/components/ui/tabs";
+import { Button } from "@/lib/components/ui/button";
+import { ForceMountTabs } from "@/lib/components/forms/force-mount-tabs";
+
 export default function ScoutingPage() {
-	return <div>Coming soon!</div>;
+	return (
+		<form
+			action={async (d) => {
+				"use server";
+				console.log(d);
+			}}
+		>
+			<ForceMountTabs defaultValue="account" className="max-w-[400px]">
+				<TabsList className="grid w-full grid-cols-2">
+					<TabsTrigger value="account">Account</TabsTrigger>
+					<TabsTrigger value="password">Password</TabsTrigger>
+				</TabsList>
+				<TabsContent value="account">
+					<div className="space-y-2">
+						<Label htmlFor="speaker_auto">Speaker Auto</Label>
+						<CounterInput name="speaker_auto" />
+					</div>
+				</TabsContent>
+				<TabsContent value="password">
+					<div className="space-y-2">
+						<Label htmlFor="speaker_teleop">Speaker Teleop</Label>
+						<CounterInput name="speaker_teleop" />
+					</div>
+				</TabsContent>
+				<Button type="submit" className="mt-4 w-full">
+					Submit
+				</Button>
+			</ForceMountTabs>
+		</form>
+	);
 }
--- lib/components/forms/force-mount-tabs/index.tsx
+++ lib/components/forms/force-mount-tabs/index.tsx
@@ -1,51 +1,50 @@
-"use client";
-
-import { HTMLAttributes, ReactElement, useState } from "react";
+import { HTMLAttributes, ReactElement } from "react";
 import { Tabs, TabsContent } from "../../ui/tabs";
-import {
-	TabsContentProps,
-	TabsListProps,
-	TabsProps,
-} from "@radix-ui/react-tabs";
+import { TabsProps } from "@radix-ui/react-tabs";
 
 type TabsP = TabsProps & HTMLAttributes<HTMLDivElement>;
 
 interface ForceMountTabsProps extends TabsP {
 	defaultValue: string;
-	children: ReactElement<TabsContentProps>[];
+	children: ReactElement[];
 }
 
+/**
+ * Force mounts all children that are instances of TabsContent
+ * @param children - The children of the ForceMountTabs component
+ * @returns Children with TabsContent components forced to be mounted
+ */
+function forceMountAllTabsContentChildren(children: ReactElement[]) {
+	return children?.map((child, i) => {
+		if (child.type !== TabsContent) return child;
+		return (
+			<TabsContent
+				{...child.props}
+				key={i}
+				value={child.props.value}
+				forceMount
+				className="data-[state=inactive]:hidden"
+			>
+				{child.props.children}
+			</TabsContent>
+		);
+	});
+}
+
+/**
+ * Extension of shadcn tabs built for multi-tabbed forms, forces TabsContent
+ * to be mounted so you don't have to worry about it!
+ *
+ * Oh...and it all works with RSC.
+ * */
 export function ForceMountTabs({
 	defaultValue,
 	children,
 	...props
 }: ForceMountTabsProps) {
-	const [activeTab, setActiveTab] = useState<string>(defaultValue);
-
 	return (
-		<Tabs
-			defaultValue={defaultValue}
-			onValueChange={setActiveTab}
-			{...props}
-		>
-			{children?.map((child, i) => {
-				const display = (
-					child.type as React.ComponentType<
-						TabsContentProps | TabsListProps
-					>
-				).displayName;
-				if (display !== "TabsContent") return child;
-				return (
-					<TabsContent
-						key={i}
-						value={child.props.value}
-						forceMount
-						hidden={activeTab !== child.props.value}
-					>
-						{child.props.children}
-					</TabsContent>
-				);
-			})}
+		<Tabs defaultValue={defaultValue} {...props}>
+			{forceMountAllTabsContentChildren(children)}
 		</Tabs>
 	);
 }
