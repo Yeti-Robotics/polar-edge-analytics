7e59702317898956827d82de82b96345682acf58
Drew Beamer <andrewmbeamer@gmail.com>
2024-07-01 14:14:43 -0400

    update drizzle-kit, add schema

--- lib/database/schema.ts
+++ lib/database/schema.ts
@@ -1,7 +1,90 @@
-import { integer, text, pgTable } from "drizzle-orm/pg-core";
+import {
+	integer,
+	text,
+	pgTable,
+	date,
+	primaryKey,
+	boolean,
+	pgSchema,
+	uuid,
+	foreignKey,
+} from "drizzle-orm/pg-core";
 
-export const frc_team = pgTable("frc_team", {
-  team_number: integer("team_number").primaryKey(),
-  team_name: text("team_name"),
-  location: text("location").notNull(),
+const authSchema = pgSchema("auth");
+
+const auth_users = authSchema.table("users", {
+	id: uuid("id").primaryKey(),
 });
+
+export const users = pgTable("users", {
+	user_id: uuid("user_id")
+		.references(() => auth_users.id)
+		.notNull()
+		.primaryKey(),
+	is_admin: boolean("is_admin").notNull().default(false),
+});
+
+export const team = pgTable("team", {
+	team_number: integer("team_number").primaryKey(),
+	team_name: text("team_name"),
+	location: text("location").notNull(),
+});
+
+export const event = pgTable("event", {
+	event_code: text("event_code").primaryKey(),
+	event_name: text("event_name").notNull(),
+	event_location: text("event_location"),
+	event_start_date: date("event_date").notNull(),
+	event_end_date: date("event_end_date").notNull(),
+});
+
+export const match = pgTable(
+	"match",
+	{
+		event_code: text("event_code")
+			.references(() => event.event_code)
+			.notNull(),
+		match_number: integer("match_number").notNull(),
+	},
+	(table) => ({
+		pk: primaryKey({ columns: [table.event_code, table.match_number] }),
+	})
+);
+
+export const stand_form = pgTable(
+	"stand_form",
+	{
+		scouter: uuid("user_id")
+			.references(() => users.user_id)
+			.notNull(),
+		team_number: integer("team_number")
+			.references(() => team.team_number)
+			.notNull(),
+		match_number: integer("match_number"),
+		event_code: text("event_code"),
+		auto_line: integer("auto_line").notNull(),
+		speaker_auto: integer("speaker_auto").notNull(),
+		amp_auto: integer("amp_auto").notNull(),
+		shuttle_auto: integer("shuttle_auto").notNull(),
+		speaker_teleop: integer("speaker_teleop").notNull(),
+		amp_teleop: integer("amp_teleop").notNull(),
+		shuttle_teleop: integer("shuttle_teleop").notNull(),
+		climbed: boolean("climbed").notNull(),
+		parked: boolean("parked").notNull(),
+		bots_on_chain: integer("bots_on_chain").notNull(),
+	},
+	(table) => ({
+		matchReference: foreignKey({
+			columns: [table.event_code, table.match_number],
+			foreignColumns: [match.event_code, match.match_number],
+		}),
+		pk: primaryKey({
+			columns: [
+				table.scouter,
+				table.team_number,
+				table.match_number,
+				table.event_code,
+			],
+		}),
+	})
+);
---
+++ supabase/migrations/0000_chief_sugar_man.sql
@@ -0,0 +1,72 @@
+CREATE TABLE IF NOT EXISTS "event" (
+	"event_code" text PRIMARY KEY NOT NULL,
+	"event_name" text NOT NULL,
+	"event_location" text,
+	"event_date" date NOT NULL,
+	"event_end_date" date NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "match" (
+	"event_code" text NOT NULL,
+	"match_number" integer NOT NULL,
+	CONSTRAINT "match_event_code_match_number_pk" PRIMARY KEY("event_code","match_number")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "stand_form" (
+	"user_id" uuid NOT NULL,
+	"team_number" integer NOT NULL,
+	"match_number" integer,
+	"event_code" text,
+	"auto_line" integer NOT NULL,
+	"speaker_auto" integer NOT NULL,
+	"amp_auto" integer NOT NULL,
+	"shuttle_auto" integer NOT NULL,
+	"speaker_teleop" integer NOT NULL,
+	"amp_teleop" integer NOT NULL,
+	"shuttle_teleop" integer NOT NULL,
+	"climbed" boolean NOT NULL,
+	"parked" boolean NOT NULL,
+	"bots_on_chain" integer NOT NULL,
+	CONSTRAINT "stand_form_user_id_team_number_match_number_event_code_pk" PRIMARY KEY("user_id","team_number","match_number","event_code")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "team" (
+	"team_number" integer PRIMARY KEY NOT NULL,
+	"team_name" text,
+	"location" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"user_id" uuid PRIMARY KEY NOT NULL,
+	"is_admin" boolean DEFAULT false NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "match" ADD CONSTRAINT "match_event_code_event_event_code_fk" FOREIGN KEY ("event_code") REFERENCES "public"."event"("event_code") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "stand_form" ADD CONSTRAINT "stand_form_user_id_users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "stand_form" ADD CONSTRAINT "stand_form_team_number_team_team_number_fk" FOREIGN KEY ("team_number") REFERENCES "public"."team"("team_number") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "stand_form" ADD CONSTRAINT "stand_form_event_code_match_number_match_event_code_match_number_fk" FOREIGN KEY ("event_code","match_number") REFERENCES "public"."match"("event_code","match_number") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "users" ADD CONSTRAINT "users_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
