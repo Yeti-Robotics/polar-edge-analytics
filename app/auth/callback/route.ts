fef0536356bdd7fb80c0f5c3744e0a6f135b29b4
Drew Beamer <andrewmbeamer@gmail.com>
2024-07-28 16:13:23 -0400

    add nickname support + cleanup

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,14 +1,37 @@
 import { createClient } from "@/lib/database/server";
+import { createServerClient } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 
-interface ServerData {
-	id: string;
-	name: string;
-	icon: string;
-	owner: boolean;
-	permissions: number;
-	permissions_new: string;
-	features: any[];
+interface DiscordGuildMemberResponse {
+	message?: string;
+	code?: number;
+	avatar: string | null;
+	banner: string | null;
+	communication_disabled_until: string | null;
+	flags: number;
+	joined_at: string;
+	nick: string;
+	pending: boolean;
+	premium_since: string | null;
+	roles: string[];
+	unusual_dm_activity_until: string;
+	user: {
+		id: string;
+		username: string;
+		avatar: string;
+		discriminator: string;
+		public_flags: number;
+		flags: number;
+		banner: string | null;
+		accent_color: string | null;
+		global_name: string;
+		avatar_decoration_data: string;
+		banner_color: string;
+		clan: string;
+	};
+	mute: boolean;
+	deaf: boolean;
+	bio: string;
 }
 
 export async function GET(request: Request) {
@@ -18,42 +41,32 @@ export async function GET(request: Request) {
 
 	if (code) {
 		const supabase = createClient();
-		await supabase.auth.exchangeCodeForSession(code);
+		const {
+			data: { session, user },
+		} = await supabase.auth.exchangeCodeForSession(code);
 
-		const { data } = await supabase.auth.getSession();
-
-		if (!data.session) {
-			return NextResponse.redirect(origin);
+		if (!session) {
+			return NextResponse.redirect(`${origin}/403`);
 		}
 
 		const headers = new Headers();
-		headers.append(
-			"Authorization",
-			`Bearer ${data.session?.provider_token}`
-		);
+		headers.append("Authorization", `Bearer ${session.provider_token}`);
 
-		let servers: ServerData[];
-		try {
-			const userServers = await fetch(
-				`https://discord.com/api/users/@me/guilds`,
-				{ headers }
-			);
-			servers = await userServers.json();
-		} catch (error) {
-			console.error("Error fetching user servers:", error);
-			return NextResponse.redirect(origin);
-		}
+		const servers: DiscordGuildMemberResponse = await fetch(
+			`https://discord.com/api/users/@me/guilds/408711970305474560/member`,
+			{ headers }
+		).then((res) => res.json());
 
-		const serverId = "408711970305474560";
-		const serverIsInList = servers.some((server) => server.id === serverId);
-
-		if (serverIsInList) {
-			// YETI is in the list of servers
-			return NextResponse.redirect(`${origin}/profile`);
-		} else {
-			// YETI is not in the list of servers
+		if (!servers || servers?.code) {
 			await supabase.auth.signOut();
-			return NextResponse.redirect(origin);
+			return NextResponse.redirect(`${origin}/403`);
 		}
+
+		await supabase
+			.from("profile")
+			.update({ nick: servers.nick })
+			.eq("id", user.id);
+
+		return NextResponse.redirect(`${origin}/profile`);
 	}
 }
