2d6c7de99119f53020062080437a58997e00a6ce
Drew Beamer <andrewmbeamer@gmail.com>
2024-09-17 21:00:29 -0400

    rm auth on page

--- app/(with-sidebar)/scout/page.tsx
+++ app/(with-sidebar)/scout/page.tsx
@@ -1,20 +1,6 @@
-"use server";
-
-import { createClient } from "@/lib/database/server";
 import { StandForm } from "../../../lib/components/stand-form/form";
 import { submitStandForm } from "./action";
-import { standFormSchema } from "@/lib/components/stand-form/schema";
 
 export default async function ScoutingPage() {
-	const supabase = createClient();
-
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
-
-	if (!user) {
-		return <div>Not wogged in</div>;
-	}
-
 	return <StandForm onSubmit={submitStandForm} />;
 }
