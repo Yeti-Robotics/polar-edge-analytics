88477163298e81dc8fd7146e06a290039512f6db
Drew Beamer <85971377+drew-beamer@users.noreply.github.com>
2024-09-19 18:46:47 -0400

    move files inside /next (#115)

--- next/next.config.js
+++ next/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
 	// Configure `pageExtensions` to include MDX files
 	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 	// Optionally, add any other Next.js config below
+	output: 'standalone'
 };
 
 module.exports = withMDX(nextConfig);
