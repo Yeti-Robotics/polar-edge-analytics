8606560fe2eff4ea36672b33f5318385f104983b
Drew Beamer <andrewmbeamer@gmail.com>
2024-10-30 18:44:45 -0400

    eslint update

---
+++ apps/storybook/.eslintrc.js
@@ -0,0 +1,30 @@
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  ignores: ['dist'],
+	root: true,
+	extends: ["@repo/eslint-config/library.js"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+	  project: true,
+	},
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
+  
