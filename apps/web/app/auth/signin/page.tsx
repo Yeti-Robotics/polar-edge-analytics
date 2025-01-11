0fb9a5f6ca94e33afc42cb3d9971a3e2b0879187
Drew Beamer <andrewmbeamer@gmail.com>
2025-01-11 16:19:01 -0500

    create nice session display page

--- apps/web/app/auth/signin/page.tsx
+++ apps/web/app/auth/signin/page.tsx
@@ -1,12 +1,16 @@
-import { discordProvider, signIn } from "../auth";
+import { signIn } from "../auth";
 
 export default function SignInPage() {
-    return (
-        <>
-            <button onClick={async () => {
-                "use server"
-                await signIn(discordProvider.id, { redirectTo: "/scout" })
-            }}>Sign in With Discord</button><br />
-        </>
-    )
+	return (
+		<>
+			<form
+				action={async () => {
+					"use server";
+					await signIn("discord", { redirectTo: "/scout" });
+				}}
+			>
+				<button type="submit">Sign in With Discord</button>
+			</form>
+		</>
+	);
 }
