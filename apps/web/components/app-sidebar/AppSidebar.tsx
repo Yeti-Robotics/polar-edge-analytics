26dc5d04b3bbf957b37277467445e638c9948d0f
vidyuthdev <vidyuth.ashok@gmail.com>
2025-01-28 20:35:56 -0500

    Create sidebar for web application

---
+++ apps/web/components/app-sidebar/AppSidebar.tsx
@@ -0,0 +1,51 @@
+import { Binoculars, Calendar, Cctv, Grid2X2, Home, Inbox, Search, Settings } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@repo/ui/components/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Scout",
+    url: "/scout",
+    icon: Binoculars,
+  },
+  {
+    title: "Data",
+    url: "/data",
+    icon: Grid2X2,
+  }
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
