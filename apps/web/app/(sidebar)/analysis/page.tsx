5f9e728dac56468875a757ca042f03cfcaebe4f1
Drew Beamer <andrewmbeamer@gmail.com>
2025-02-17 17:26:55 -0500

    force dynamic

--- apps/web/app/(sidebar)/analysis/page.tsx
+++ apps/web/app/(sidebar)/analysis/page.tsx
@@ -1,30 +1,28 @@
 import { scoutedTeamData } from "./actions";
 import { TeamDataTable } from "./components/TeamDataTable";
 
-
 export default async function TeamData() {
-    const teamDataResult = await scoutedTeamData();
+	const teamDataResult = await scoutedTeamData();
 
-    return (
-        <div className="flex flex-col">
-            <h1 className="text-2xl md:text-3xl font-semibold leading-none tracking-tight pl-5 md:pl-0">
-                Team Data
-            </h1>
-            <div className="mx-1 my-4">
-                {!teamDataResult.success
-                    ? "Error fetching team data"
-                    : teamDataResult.value.length < 1
-                        ? "No team data yet"
-                        : ""}
-            </div>
-            <div>
-                {teamDataResult.success && (
-                    <TeamDataTable teamData={teamDataResult.value} />
-                )}
-            </div>
-        </div>
-    )
+	return (
+		<div className="flex flex-col">
+			<h1 className="text-2xl md:text-3xl font-semibold leading-none tracking-tight pl-5 md:pl-0">
+				Team Data
+			</h1>
+			<div className="mx-1 my-4">
+				{!teamDataResult.success
+					? "Error fetching team data"
+					: teamDataResult.value.length < 1
+						? "No team data yet"
+						: ""}
+			</div>
+			<div>
+				{teamDataResult.success && (
+					<TeamDataTable teamData={teamDataResult.value} />
+				)}
+			</div>
+		</div>
+	);
 }
 
-
-
+export const dynamic = "force-dynamic";
