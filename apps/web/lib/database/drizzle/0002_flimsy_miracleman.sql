d081fe33fa49c65b0048a5452024a2d21f826c13
Drew Beamer <andrewmbeamer@gmail.com>
2025-02-25 19:20:27 -0500

    fix schema validation error when penalties are not present

---
+++ apps/web/lib/database/drizzle/0002_flimsy_miracleman.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "match_score_breakdown" ALTER COLUMN "g206_penalty" SET DEFAULT false;--> statement-breakpoint
+ALTER TABLE "match_score_breakdown" ALTER COLUMN "g408_penalty" SET DEFAULT false;--> statement-breakpoint
+ALTER TABLE "match_score_breakdown" ALTER COLUMN "g424_penalty" SET DEFAULT false;
