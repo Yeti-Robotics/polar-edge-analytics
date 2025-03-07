673e1421160aaef4ce6b9d58627002ed8f4ee0b4
Drew Beamer <andrewmbeamer@gmail.com>
2025-03-06 20:28:56 -0500

    restart

--- apps/web/app/(sidebar)/scout/components/utils.ts
+++
@@ -1,9 +0,0 @@
-export const prettyPrint = (string: string) =>
-	string
-		.split("_")
-		.map((s) => `${s.charAt(0).toUpperCase()}${s.slice(1)}`)
-		.join(" ");
-
-export const pascalToSpace = (string: string) => {
-	return string.replace(/([A-Z])/g, ' $1');
-}
