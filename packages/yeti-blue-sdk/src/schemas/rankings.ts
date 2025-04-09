9fc16bc5c6946e7aa28d38515060edd4ec5223a2
AnirvinP <anirvin421potaraju@gmail.com>
2025-04-08 20:59:23 -0400

    add schemas

--- packages/yeti-blue-sdk/src/schemas/rankings.ts
+++ packages/yeti-blue-sdk/src/schemas/rankings.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+
 export const DistrictEventPointsSchema = z.object({
     district_cmp: z.boolean(),
     total: z.number().min(0),
@@ -8,6 +9,38 @@ export const DistrictEventPointsSchema = z.object({
     award_points: z.number().min(0),
     event_key: z.string(),
     qual_points: z.number().min(0)
+})
+
+
+export const ListOfRankingsSchema = z.object({
+    matches_played: z.number().min(0),
+    qual_average: z.number().min(0).nullable(),
+    extra_stats: z.array(z.any()),
+    sort_orders: z.number().min(0).nullable(),
+    record: z.object({
+        wins: z.number().min(0),
+        losses: z.number().min(0),
+        ties: z.number().min(0),
+    }).nullable(),
+    rank: z.number().positive(),
+    team_key: z.string().regex(/^frc\d+$/),
+    dq: z.number().min(0)      
+})
+
+export const ExtraStatsInfoSchema = z.object({
+    precision: z.number().min(0),
+    name: z.string().regex(/^frc\d+$/)
+})
+
+export const SortOrderInfoSchema = z.object({
+    precision: z.number().min(0),
+    name: z.string().regex(/^frc\d+$/)
+})
+
+export const EventRankingsSchema = z.object({
+    rankings: z.array(ListOfRankingsSchema),
+    extra_stats_info: z.array(ExtraStatsInfoSchema),
+    sort_order_info: z.array(SortOrderInfoSchema)
 })
 
 export const DistrictRankingSchema = z.object({
@@ -29,3 +62,4 @@ export const RegionalPoolRankingSchema = z.object({
 
 export type DistrictRanking = z.infer<typeof DistrictRankingSchema>
 export type RegionalPoolRanking = z.infer<typeof RegionalPoolRankingSchema>
+export type EventRankings = z.infer<typeof EventRankingsSchema>
