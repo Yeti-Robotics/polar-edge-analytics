b22cdec2bbcc0e8c3bd59b1ee017094ce39a3502
AnirvinP <anirvin421potaraju@gmail.com>
2025-04-10 21:55:40 -0400

    added more queries

--- apps/web/app/(sidebar)/team/[[...teamNumber]]/page.tsx
+++ apps/web/app/(sidebar)/team/[[...teamNumber]]/page.tsx
@@ -11,24 +11,28 @@ interface TeamPageProps {
 
 export default async function TeamPage({ params }: TeamPageProps) {
     const {teamNumber} = await params;
-
-    const [autoEPA] = await db.select({
+    const [teamInfo] = await db.select().from(team).where(eq(team.teamNumber, parseInt(teamNumber))).limit(1);
+    const [teamStats] = await db.select({
         autoCoral: sql`AVG(${teamMatchStats.autoCoralLevel1} + ${teamMatchStats.autoCoralLevel2} + ${teamMatchStats.autoCoralLevel3} + ${teamMatchStats.autoCoralLevel4})`.mapWith(Number).as("autoCoral"),
         autoAlgae: sql`AVG(${teamMatchStats.autoAlgaeNet} + ${teamMatchStats.autoAlgaeProcessor})`.mapWith(Number).as("autoAlgae"),
+        autoCoralPoints: sql`AVG(${teamMatchStats.autoCoralLevel1} * 3 + ${teamMatchStats.autoCoralLevel2} * 4 + ${teamMatchStats.autoCoralLevel3} * 6 + ${teamMatchStats.autoCoralLevel4} * 7)`.mapWith(Number).as("autoCoralPoints"),
     }).from(teamMatchStats)
         .groupBy(teamMatchStats.teamNumber)
         .having(eq(teamMatchStats.teamNumber, parseInt(teamNumber))
     )
 
-    if (!autoEPA) {
+    // const teamStandForms = await db.select().from(standForm).where(eq(standForm.teamNumber, parseInt(teamNumber)));
+    
+    
+
+    if (!teamStats || !teamInfo) {
         return <div>Team not found</div>
     }
 
-    console.log(autoEPA)
     return <div>
-        <h1>{teamNumber}</h1>
+        <h1 className = "text-lg font-bold italic">{teamNumber} - {teamInfo.teamName}</h1>
         <section>
-            <h2>{autoEPA.autoAlgae} {autoEPA.autoCoral}</h2>
+            <h2>{teamStats.autoAlgae} {teamStats.autoCoral}</h2>
             <Button>Button</Button>
             
         </section>
