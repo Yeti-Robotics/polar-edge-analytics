d97d08fd3184b6b4c652393d090cc094b4851617
Drew Beamer <andrewmbeamer@gmail.com>
2025-04-17 00:14:22 -0400

    dont run migration

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,7 +1,7 @@
 export async function register() {
 	// eslint-disable-next-line turbo/no-undeclared-env-vars
-	if (process.env.NEXT_RUNTIME === "nodejs") {
-		console.log("running migration...");
-		await import("./instrumentation.node");
-	}
+	// if (process.env.NEXT_RUNTIME === "nodejs") {
+	// 	console.log("running migration...");
+	// 	await import("./instrumentation.node");
+	// }
 }
