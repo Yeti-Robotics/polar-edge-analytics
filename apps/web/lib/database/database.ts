73c2fa88d28d33dce6269fc7dfb053ea49e8c9a9
Drew Beamer <andrewmbeamer@gmail.com>
2025-04-17 00:04:44 -0400

    refactor: simplify database initialization by removing unnecessary object wrapping

--- apps/web/lib/database/database.ts
+++ apps/web/lib/database/database.ts
@@ -1,9 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const db = drizzle({
-	client: process.env.DATABASE_URL,
-	schema,
-});
+const db = drizzle(process.env.DATABASE_URL!, { schema });
 
 export default db;
