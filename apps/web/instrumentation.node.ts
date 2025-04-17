3ffb9cdc6072ed54fe5e71b072ddef9cdfa8a872
Drew Beamer <andrewmbeamer@gmail.com>
2025-04-17 00:02:36 -0400

    switch to neon http migrator

--- apps/web/instrumentation.node.ts
+++ apps/web/instrumentation.node.ts
@@ -1,7 +1,7 @@
 import path from "path";
 
 import { db } from "@/lib/database";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { migrate } from "drizzle-orm/neon-http/migrator";
 
 const migrationsFolder = path.join(process.cwd(), "lib/database/drizzle");
 
